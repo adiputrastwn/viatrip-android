cb4d86ba52a5c98cb6ad2bea1b960be712e4746c
Adi Putra <adiputrastwn@gmail.com>
2022-03-26 10:27:06 +0700

    Setup Navigation Component with safeargs plugin

--- app/src/main/java/com/adiputrastwn/viatrip/DetailTripFragment.kt
+++ app/src/main/java/com/adiputrastwn/viatrip/DetailTripFragment.kt
@@ -6,34 +6,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.adiputrastwn.viatrip.databinding.FragmentFirstBinding
+import com.adiputrastwn.viatrip.databinding.FragmentDetailTripBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-class FirstFragment : Fragment() {
+class DetailTripFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: FragmentDetailTripBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+    ): View {
+        _binding = FragmentDetailTripBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        with(binding) {
+            buttonSecond.setOnClickListener {
+                DetailTripFragmentDirections.showHomeScreen().also {
+                    findNavController().navigate(it)
+                }
+            }
         }
     }
 
--- app/src/main/java/com/adiputrastwn/viatrip/HomeFragment.kt
+++ app/src/main/java/com/adiputrastwn/viatrip/HomeFragment.kt
@@ -6,34 +6,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.adiputrastwn.viatrip.databinding.FragmentSecondBinding
+import com.adiputrastwn.viatrip.databinding.FragmentHomeBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
-class SecondFragment : Fragment() {
+class HomeFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        with(binding) {
+            buttonFirst.setOnClickListener {
+                HomeFragmentDirections.showDetailTrip().also {
+                    findNavController().navigate(it)
+                }
+            }
         }
     }
 
